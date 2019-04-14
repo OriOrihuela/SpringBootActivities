0af5b81618873dc026a560aa57e10852f7c29205
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-14 15:03:03 +0200

    @Controller to class; @Autowired property servei added

--- exercise4/src/main/java/org/formacio/mvc/Controlador.java
+++ exercise4/src/main/java/org/formacio/mvc/Controlador.java
@@ -1,5 +1,14 @@
 package org.formacio.mvc;
 
+import org.formacio.component.ServeiAlumnat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class Controlador {
+	
+	/* ---- Properties ---- */
+	@Autowired
+	private ServeiAlumnat servei = null;
 
 }
