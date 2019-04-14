8088a7aebf68f320b62e29a20a75c0d6db8f633d
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-14 15:22:48 +0200

    @RestController class done :D

--- exercise4/src/main/java/org/formacio/mvc/Controlador.java
+++ exercise4/src/main/java/org/formacio/mvc/Controlador.java
@@ -2,13 +2,33 @@ package org.formacio.mvc;
 
 import org.formacio.component.ServeiAlumnat;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class Controlador {
 	
 	/* ---- Properties ---- */
 	@Autowired
 	private ServeiAlumnat servei = null;
-
+	
+	
+	/* ---- Constructor ---- */
+	public Controlador (ServeiAlumnat servei) {
+		this.servei = servei;
+	}
+	
+	
+	/* ---- Getters ---- */
+	public ServeiAlumnat getServei() {
+		return servei;
+	}
+	
+	
+	/* ---- Behaviours ---- */
+	@RequestMapping(value = "/alumnes", method = RequestMethod.GET)
+	public int devolverNumeroAlumnes() {
+		return getServei().getRepositori().getBBDD().size();
+	}
 }
